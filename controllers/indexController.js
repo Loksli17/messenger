901a44c41c65f673c7b00b8422bc1d5a0cfdb0fe
Loksli <45991921+RobinLoksli@users.noreply.github.com>
2020-01-29 17:34:38 +1000

    show chats

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,12 +1,47 @@
 const mongoose  = require('./../lib/database/mongoose');
 //models
 const ChatModel = require('./../models/ChatModel');
+const UserModel = require('./../models/UserModel');
+
 
 exports.actionIndex = async (req, res) => {
-    console.log(mongoose.version);
 
-    let userChats = await ChatModel.find({['users.' + req.session.userIndentity._id]: req.session.userIndentity._id});
-    console.log(userChats);
+    let userChats       = await ChatModel.find({['users.' + req.session.userIndentity._id]: req.session.userIndentity._id}).limit(2),
+        countActiveChat = 0;
 
-    res.render('index');
+    if(!userChats.length){
+        res.render('index', {
+            error: true,
+        });
+        return;
+    }
+
+    for(let i = 0; i < userChats.length; i++){
+        userChats[i].lastMes = userChats[i].messages[0];
+
+
+        if(userChats[i].lastMes != undefined){
+            userChats[i].lastMes.user = await UserModel.findOne({_id: userChats[i].lastMes.userId});
+            userChats[i].isActive = true;
+            countActiveChat++;
+        }else{
+            userChats[i].isActive = false;
+        }
+
+        delete userChats[i].users;
+        delete userChats[i].messages;
+
+        for(let key in userChats[i].users){
+            if(key != req.session.userIndentity._id){
+                userChats[i].link = key;
+            }
+        }
+
+        console.log();
+    }
+
+    res.render('index', {
+        error: countActiveChat ? false : true,
+        chats: userChats,
+    });
 };
